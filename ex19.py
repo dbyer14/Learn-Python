13c9aee5d37ef32a15e5089e7915dd9518602906
dbyer14 <dbyer14@users.noreply.github.com>
2015-01-31 18:13:44 -0800

    Adds ex20.py, prior to study drills but with comments

--- ex19.py
+++ ex19.py
@@ -1,25 +1,27 @@
+
+# this creates the function cheese_and_crackers, which takes two variables. the first variable is the number of cheeses, the second is the number of boxes of crackers
 def cheese_and_crackers(cheese_count, boxes_of_crackers):
 	print "You have %d cheeses!" % cheese_count
 	print "You have %d boxes of crackers!" % boxes_of_crackers
 	print "Man that's enough for a party!"
 	print "Get a blanket. \n"
 
-
-print "we can just give the function numbers directly:"
+# this line calls the function and uses numbers for the variables directly
+print "We can just give the function numbers directly:"
 cheese_and_crackers(20, 30)
 
-
+# these lines asign numbers to variabls, and then pass the variables to the function cheese_and_crackers
 print "OR, we can use variables from our script:"
 amount_of_cheese = 10
 amount_of_crackers = 50
 
 cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 
-
+# this linke shows that you can do math within the function durectky
 print "We can even do math inside too:"
-cheese_and_crackers(10 + 5, 5 + 6)
+cheese_and_crackers(10 + 20, 5 + 6)
 
-
+# this line shows that math between named variables and numbers can be completed within funtions
 print "And we can combine the two, variables and math:"
 cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
 
---
+++ ex20.py
@@ -0,0 +1,54 @@
+
+# imports argv from the sys module
+from sys import argv
+
+# assigns variables to argv, in this case only a single file name variable is assigned
+script, input_file = argv
+
+# this function prints out the contents of a file
+def print_all(f):
+	print f.read()
+
+# this function moves to the first location (0) in the file
+def rewind(f):
+	f.seek(0)
+	
+""" 
+this function prints a single line. it takes in a variable and prints that variable, 
+then in calls the "readline" function on the file, f. I think that the "cursor" or where 
+we are in the files memorory moves each time a line is read. So, when readline() is called 
+in sucession, it will read subsequent lines. Need to confirm.
+"""
+
+def print_a_line(line_count, f):
+	print line_count, f.readline()
+
+	
+# this line opens the file the user input when initially running the script and assigns the file object to a variable, 'current_file'
+current_file = open(input_file)
+
+print "First let's print the whole file:\n"
+
+# this line prints the whole file
+print_all(current_file)
+
+print "Now let's rewind, kind of like a tape."
+
+# this line returns the "cursor/memory" to the beginning of the file
+rewind(current_file)
+
+print "Let's print three lines:"
+
+# this line assigns a value to the variable current line
+current_line = 1
+
+# this line runs the function print_a_line on two variables, current_line (an integer) and current_file (a file object)
+print_a_line(current_line, current_file)
+
+#  this line increments the value of current line and runs the print_a_line function
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+#  this line increments the value of current line and runs the print_a_line function
+current_line = current_line + 1
+print_a_line(current_line, current_file)
